a9a8b7f0a868e225a11ad93a1a11fa7bc5a5aa7a
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2023-06-27 13:38:17 -0700

    Camera & Player rotations

--- Assets/Resources/Scripts/PlayerCamera.cs
+++ Assets/Resources/Scripts/PlayerCamera.cs
@@ -7,20 +7,41 @@ public class PlayerCamera : MonoBehaviour
     //The player object this camera is attached to
     private GameObject player;
     private int rotation_sensitivity = 10;
+    private const float ROTATION_LIMIT = 60f;
+
+    //Rotation values for the camera
+    private float xRotation = 0f;
+    private float yRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x_axis = Input.GetAxis("Mouse X");
-        float y_axis = Input.GetAxis("Mouse Y");
-        player.transform.Rotate(Vector3.up * x_axis * rotation_sensitivity);
+        Rotation(ROTATION_LIMIT);
     }
 
+    /// <summary>
+    /// Local camera rotation along x-axis (up-down)
+    /// and local player rotation along y-axis (left-right).
+    /// Should be called in every frame
+    /// </summary>
+    /// <param name="rotationLimit">
+    /// The upper and lower angle bounds of x-axis rotation of the camera
+    /// </param>
+    private void Rotation(float rotationLimit)
+    {
+        xRotation += Input.GetAxis("Mouse X") * rotation_sensitivity;
+        yRotation -= Input.GetAxis("Mouse Y") * rotation_sensitivity;
+        //Limits the x-axis rotation of the camera
+        yRotation = Mathf.Clamp(yRotation, -rotationLimit, rotationLimit);
 
+        player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, xRotation, 0f);
+        transform.eulerAngles = new Vector3(yRotation, transform.eulerAngles.y, 0f);
+    }
 }
