90f6f046f3c85bd52e508f29eb9328c91b898a46
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2023-06-27 12:56:48 -0700

    managable open/close door actions

--- Assets/Resources/Scripts/DoorHandler.cs
+++ Assets/Resources/Scripts/DoorHandler.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DoorHandler : MonoBehaviour
 {
-    private const string OPENABLE_TAG = "OpenableDoor";
+    //private const string OPENABLE_TAG = "OpenableDoor";
 
     private const string PLAYER_TAG = "Player";
+    private const string OPEN_STATE = "OpenDoor";
+    private const string CLOSE_STATE = "CloseDoor";
     private const string OPEN_TRIGGER = "tr_open_door";
     private const string CLOSE_TRIGGER = "tr_close_door";
     private const string LOCKED_TRIGGER = "tr_locked_door";
@@ -16,6 +18,7 @@ public class DoorHandler : MonoBehaviour
     private bool locked;
     [SerializeField]
     private bool opened;
+    [SerializeField]
     private bool forever_locked;
 
     private Animator parent_animator;
@@ -25,16 +28,16 @@ public class DoorHandler : MonoBehaviour
         //Due to the scale of the project, it is assumed animator is supplied
         parent_animator = GetComponentInParent<Animator>();
 
-        //Handle the doors that should not be ever opened by the player
-        if (transform.parent.CompareTag(OPENABLE_TAG))
-        {
-            forever_locked = false;
-        }
-        //Other openable doors
-        else
-        {
-            forever_locked = true;
-        }
+        ////Handle the doors that should not be ever opened by the player
+        //if (transform.parent.CompareTag(OPENABLE_TAG))
+        //{
+        //    forever_locked = false;
+        //}
+        ////Other openable doors
+        //else
+        //{
+        //    forever_locked = true;
+        //}
 
         
         //Handling the intitial state defined in inspector
@@ -48,48 +51,46 @@ public class DoorHandler : MonoBehaviour
         }
     }
 
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        //Plays the openning door animation
-        if (collision.gameObject.CompareTag(PLAYER_TAG))
-        {
-            Debug.Log("Openning the door");
-            OpenClose();
-        }
-    }
-
     /// <summary>
     /// Attempts to open or close the door.
     /// </summary>
     /// <returns>
     /// True if door was opened or closed successfully.
     /// </returns>
-    private bool OpenClose()
+    public bool OpenClose()
     {
         Debug.Log("Open/Close called");
 
-        Debug.Log("Is locked: " + locked);
-        Debug.Log("Is forever locked: " + forever_locked);
+        //Debug.Log("Is locked: " + locked);
+        //Debug.Log("Is forever locked: " + forever_locked);
 
         //Don't try if the door is locked
         if (forever_locked || locked)
             return false;
 
-        Debug.Log("is not locked");
+        parent_animator.ResetTrigger(CLOSE_TRIGGER);
+        parent_animator.ResetTrigger(OPEN_TRIGGER);
+
+        //Debug.Log("is not locked");
         //Close if the door is opened
-        if (opened)
+        if (parent_animator)
         {
-            parent_animator.SetTrigger(CLOSE_TRIGGER);
-            opened = false;
+            if (parent_animator.GetCurrentAnimatorStateInfo(0).IsName(OPEN_STATE))
+            {
+                Debug.Log("Closing...");
+                parent_animator.SetTrigger(CLOSE_TRIGGER);
+                
+                opened = false;
+            }
+            //Open if the door is closed
+            else if(parent_animator.GetCurrentAnimatorStateInfo(0).IsName(CLOSE_STATE))
+            {
+                Debug.Log("Openning...");
+                parent_animator.SetTrigger(OPEN_TRIGGER);
+
+                opened = true;
+            }
         }
-        //Open if the door is closed
-        else
-        {
-            parent_animator.SetTrigger(OPEN_TRIGGER);
-            opened = true;
-        }
-        //Debug.Log("Current Animation: " + parent_animator.GetCurrentAnimatorClipInfo(0));
 
         return true;
     }
--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    //A tag for player's camera retrieval
+    private const string CAMERA_TAG = "PlayerCamera";
+    //A camera for player, acting as player's "eyes"
+    private GameObject eyes;
+    ////A ray coming from player's "eyes"
+    //private Physics vision;
     private Rigidbody rb;
     private int speed;
+    //Player's movement input on x-axis
     float x_move;
+    //Player's movement input on z-axis
     float z_move;
 
     // Start is called before the first frame update
     void Start()
     {
+        eyes = gameObject.transform.GetChild(0).gameObject;
+        if(!eyes.CompareTag(CAMERA_TAG))
+        {
+            eyes = null;
+        }
         rb = GetComponent<Rigidbody>();
         speed = 5;
     }
@@ -19,8 +33,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
     }
+
+    /// <summary>
+    /// Performs an acting on the object received after raycasting
+    /// </summary>
+    private void Interact()
+    {
+        Vector3 origin = eyes.transform.position;
+        Vector3 direction = eyes.transform.forward;
+
+        //Debug.Log("Rotation: " + direction);
+        Debug.DrawRay(origin, direction, Color.red);
+
+        bool hit = Physics.Raycast(origin, direction, out RaycastHit ray_info);
+        DoorHandler doorHandler = ray_info.collider.gameObject.GetComponent<DoorHandler>();
+
+        //Tries to open/close the door if possible
+        if (hit && doorHandler)
+        {
+            doorHandler.OpenClose();
+        }
+    }
+
+    //private IEnumerator<WaitForSeconds> DeleteTimer(Component toDelete, float time)
+    //{
+    //    yield return new WaitForSeconds(time);
+
+    //    Destroy(toDelete);
+    //}
 
     // FixedUpdate is called once per fixed frame
     private void FixedUpdate()
